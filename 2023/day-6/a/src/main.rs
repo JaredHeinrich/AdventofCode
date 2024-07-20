964188f440cc5a7921053c0a2355a8a868af9b04
Jared Heinrich <jared.heinrich@sap.com>
2024-07-20 14:09:00 +0200

    day-6

---
+++ 2023/day-6/a/src/main.rs
@@ -0,0 +1,36 @@
+use core::f32;
+use std::{fs, ops::Div, u32};
+
+fn main() {
+    let input: String = fs::read_to_string("../input.txt").unwrap();
+    let races: Vec<Vec<u32>> = input
+        .lines()
+        .map(|line| line
+            .split(":")
+            .nth(1)
+            .unwrap()
+            .trim()
+            .split(" ")
+            .filter(|e|!e.is_empty())
+            .map(|n|n.parse::<u32>().unwrap())
+            .collect())
+        .collect();
+    println!("result: {}", one(&races));
+}
+
+fn one(races: &Vec<u32>) -> u32{
+    let number_races = races[0].len();
+    let mut result = 1;
+    for i in 0..number_races {
+        let (l,u) = calculate_bounds(races[0][i], races[1][i]);
+        result = result*(u-l+1);
+    }
+    result
+}
+
+fn calculate_bounds(time: u32, distance: u32) -> (u32, u32){
+    let tmp:f32 = ((time.pow(2)-4*distance) as f32).sqrt();
+    let lower: u32 = ((time as f32 - tmp).div(2.0)+1.0).floor() as u32;
+    let upper: u32 = ((time as f32 + tmp).div(2.0)-1.0).ceil() as u32;
+    (lower, upper)
+}
---
+++ 2023/day-6/b/src/main.rs
@@ -0,0 +1,31 @@
+use std::{fs, ops::Div};
+
+fn main() {
+    let input: String = fs::read_to_string("../input.txt").unwrap();
+    let races: Vec<u64> = input
+        .lines()
+        .map(|line| line
+            .split(":")
+            .nth(1)
+            .unwrap()
+            .trim()
+            .chars()
+            .filter(|e|!(*e == ' '))
+            .collect::<String>()
+            .parse::<u64>()
+            .unwrap()
+        ).collect();
+    println!("result: {}", one(&races));
+}
+
+fn one(races: &Vec<u64>) -> u64{
+    let (l,u) = calculate_bounds(races[0], races[1]);
+    u-l+1
+}
+
+fn calculate_bounds(time: u64, distance: u64) -> (u64, u64){
+    let tmp:f64 = ((time.pow(2)-4*distance) as f64).sqrt();
+    let lower: u64 = ((time as f64 - tmp).div(2.0)+1.0).floor() as u64;
+    let upper: u64 = ((time as f64 + tmp).div(2.0)-1.0).ceil() as u64;
+    (lower, upper)
+}
