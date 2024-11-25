569277983a7a4114631838f810cae387cfddb30e
Jared Heinrich <jared.heinrich@sap.com>
2024-11-25 17:38:30 +0100

    Solve 2022 day-01-a

---
+++ 2022/Haskell/day-01-a/src/Main.hs
@@ -0,0 +1,19 @@
+parse :: String -> [[Int]]
+parse = map (map read) . splitAtEmptyLine . lines
+
+splitAtEmptyLine :: [String] -> [[String]]
+splitAtEmptyLine [] = []
+splitAtEmptyLine xs = first : splitAtEmptyLine (drop 1 rest)
+    where
+    (first, rest) = break null xs
+
+solve :: [[Int]] -> Int
+solve = maximum . map sum
+
+main :: IO ()
+main = do
+    let filepath :: String = "../../inputs/input-01.txt"
+    input :: String <- readFile filepath
+    let parsed :: [[Int]] = parse input
+    let solution :: Int = solve parsed
+    print solution
